6524a1a284679fe7f5a81cc7563fc0cc3d670f61
petar <petar.dmnv@gmail.com>
2022-02-03 19:01:06 +0200

    Add changes to API documentation

--- Backend/main.js
+++ Backend/main.js
@@ -6,108 +6,225 @@ const swaggerUI = require("swagger-ui-express");
 
 
 
-app.use( express.json() )
+app.use(express.json())
 app.listen(PORT);
 
 
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-      termsOfService: "http://example.com/terms/",
-      contact: {
-        name: "API Support",
-        url: "http://www.exmaple.com/support",
-        email: "support@example.com",
-      },
-    },
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "DEX for NFTs",
+            version: "1.0.0",
+            description: "A decentralized application for nft exchanges",
+            contact: {
+                name: "Petar Borisov",
+            },
+        },
 
-    servers: [
-      {
-        url: "http://localhost:4001",
-        description: "My API Documentation",
-      },
-    ],
-  },
-  apis: ["./Routes/*.js"],
+        servers: [{
+            url: "http://localhost:8080",
+            description: "My API Documentation",
+        }, ],
+    },
+    apis: ["./main.js"],
 };
 
 const specs = swaggerJsDoc(options);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 
-app.post('/register', (req, res) =>{
-    const { username, password, email } = req.body;
-    // create user and add it to db
-    // 
 
+
+/** 
+ * @swagger
+ * /register:
+ *   post:
+ *     description: register to the site
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                password:
+ *                  type: string
+ *                name:
+ *                  type: string
+ *                email:
+ *                  type: string
+ *            examples:
+ *              '0':
+ *                value: |-
+ *                  {
+ *                      "name": "Dido",
+ *                      "email": "template@at.com",
+ *                      "password": "pass"
+ *                  }
+ *     responses:
+ *       '201':
+ *         description: User registered
+ */
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    // create user and add it to db
     res.status(201).send({
-        text: `User ${username} registered!`
+        text: `User ${name} registered`
     });
-    
 });
 
-app.post('/login', (req, res) =>{
-    const { username, password, email } = req.body;
+
+
+
+/** 
+ * @swagger
+ * /login:
+ *   post:
+ *     description: login to the site
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                password:
+ *                  type: string
+ *                email:
+ *                  type: string
+ *            examples:
+ *              '0':
+ *                value: |-
+ *                  {
+ *                      "email": "template@at.com",
+ *                      "password": "pass"
+ *                  }
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '401':
+ *         description: Invalid credentials
+ */
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
     // check if user exists
     // 
-    user = {}
-
-    if(user){
+    user = {};
+    if (user) {
         res.status(200).send({
-            text: `Logged in ${username}!`
+            text: `Logged in ${email}`
         });
     }
-
     // if user provides invalid credentials
     res.sendStatus(401);
-
 });
 
 
-app.get('/nfts', (req, res) =>{
 
 
+/** 
+ * @swagger
+ * /nfts:
+ *   get:
+ *     description: Get all nfts for sale
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+app.get('/nfts', (req, res) => {
     // get nfts list from db
     nfts = [];
     res.status(200).send({
         nfts: nfts
     });
-
 });
 
-app.get('/nft/:id', (req, res) =>{
-    const { id } = req.params
 
+
+
+/** 
+ * @swagger
+ * /home:
+ *   get:
+ *     description: Get related nfts for sale
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+app.get('/home', (req, res) => {
+    // get nfts list from db
+    nfts = [];
+    res.status(200).send({
+        nfts: nfts
+    });
+});
+
+/** 
+ * @swagger
+ * /nft/{id}:
+ *   get:
+ *     description: Get all nfts for sale
+ *     parameters:
+ *       - name: id
+ *         in: query
+ *         schema:
+ *           type: integer
+ *         example: 1
+ *     responses:
+ *       '200':
+ *         description: Created
+ *       '404':
+ *         description: Not found
+ */
+app.get('/nft/:id', (req, res) => {
+    const { id } = req.params;
     // get nft with given id from db
-    nft = {}
+    nft = {};
 
-    if(nft){ 
+    if (nft) {
         res.status(200).send({
             nft: nft
         });
     }
-
     res.sendStatus(404);
-
-
 });
 
-// buy -- remove nft from list
-app.post('/nft/:id/buy', (req, res) =>{
-    const { id } = req.params
-    
+/** 
+ * @swagger
+ * /nft/{id}/buy:
+ *   post:
+ *     description: buy current nft
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                password:
+ *                  type: string
+ *                email:
+ *                  type: string
+ *            examples:
+ *              '0':
+ *                value: |-
+ *                  {
+ *                      "email": "template@at.com",
+ *                      "password": "pass"
+ *                  }
+ *     responses:
+ *       '201':
+ *         description: Auto generated using Swagger Inspector
+ *       '401':
+ *         description: Invalid credentials
+ */
+app.post('/nft/:id/buy', (req, res) => {
+    const { id } = req.params;
+    // buy -- remove nft from list
     // get nft with this id from db
-    nft = {}
-
-    //  check if buyer has enough
-    //  money to buy nft from wallet
-    // remove nft from db
-
+    nft = {};
+    // check if buyer has enough money to buy nft from wallet
+    // change nft owners and other paramerers for ownership
+    // change nft status
     //success
     res.sendStatus(200);
     // failure
@@ -116,10 +233,13 @@ app.post('/nft/:id/buy', (req, res) =>{
 });
 
 
+
+
+
 // sell -- add nft to list
-app.post('/sell', (req, res) =>{
-    const { nft_name,  description, nft_address, image, seller_name, nft_price } = req.body;
-    
+app.post('/sell', (req, res) => {
+    const { name, description, address, image, owner, price } = req.body;
+
     // check if user has this nft(contract level)
     // add nft to db
 
@@ -130,25 +250,105 @@ app.post('/sell', (req, res) =>{
 
 });
 
-// settings 
-app.get('/settings', (req, res) =>{
-    
-    // get user from db
-    userInfo = {};
 
-    res.send({
-        userInfo: userInfo
+
+
+/** 
+ * @swagger
+ * /settings:
+ *   get:
+ *     description: Get user/account ettings
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+app.get('/settings', (req, res) => {
+
+    // get user name and email from db
+    usernameEmailData = {};
+
+    res.sendStatus(200).send({
+        accountInfo: usernameEmailData
     });
 
 });
-// 
 
-app.post('/deleteAccount', (req, res) =>{
-    // get currentUser from db
 
-    //remove from db
-    
-    res.sendStatus(200);
+
+
+
+app.put('/settings/change-password', (req, res) => {
+
+    // remove user if exists
+    res.status(200).send({
+        text: `Removed user`
+    });
+    res.sendStatus(400);
+
 });
 
 
+
+
+app.delete('/deleteAccount', (req, res) => {
+    // get currentUser from db
+
+    //remove from db
+    res.status(200).send({
+        text: `Removed user`
+    });
+    res.sendStatus(400);
+});
+
+
+
+
+/** 
+ * @swagger
+ * /my-nfts:
+ *   get:
+ *     description: Get user nfts for sale
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+app.get('/my-nfts', (req, res) => {
+    // get nfts list from db which the current user owns
+    nfts = [];
+    res.status(200).send({
+        nfts: nfts
+    });
+
+});
+
+
+app.delete('/nft/:id/remove-forsale', (req, res) => {
+    // How to overuse nft route
+    // If user has access to the choosen nft then he can remove it from sale's list
+    nft = [];
+    res.status(200).send({
+        text: `Removed from sale`
+    });
+    res.sendStatus(404);
+
+});
+
+/** 
+ * @swagger
+ * /account:
+ *   get:
+ *     description: Get account properties
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+app.get('/account', (req, res) => {
+
+    // get user from db
+    account = {};
+
+    res.status(200).send({
+        accountInfo: account
+    });
+
+});
