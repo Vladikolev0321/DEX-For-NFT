e75d385b96210dd0797aa624f47652755addd39d
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-02-04 15:25:24 +0200

    Added tests for register route

---
+++ Backend/test/test.js
@@ -0,0 +1,48 @@
+const { assert } = require("chai");
+const chai = require("chai");
+let chaiHttp = require("chai-http");
+const { type } = require("express/lib/response");
+let server = require("../main.js");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("NFT-DEX", () => {
+
+    describe('/POST register', () => {
+        it('it should register a user', (done) => {
+            let user = {
+                name: "Dido",
+                email: "template@at.com",
+                password: "pass"
+            }
+          chai.request(server)
+              .post('/register')
+              .send(user)
+              .end((err, res) => {
+                    assert(res.status === 201);
+                    res.should.to.be.json;
+                    assert(res.body['text'] === `User ${user['name']} registered`);
+                done();
+              });
+        });
+
+        it('it should return 401 status code for missing credentials', (done) => {
+            let user = {}
+          chai.request(server)
+              .post('/register')
+              .send(user)
+              .end((err, res) => {
+                    assert(res.status === 401);
+                done();
+              });
+        });
+
+    });
+
+
+
+
+});
+
+
